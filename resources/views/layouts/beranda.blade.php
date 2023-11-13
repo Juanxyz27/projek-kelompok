4953e2f94438e0afac507dc7bbda32d79fecd524
Juanxyz27 <kiflyjuan277@gmail.com>
2023-11-14 01:22:07 +0700

    pembaruan1

--- resources/views/layouts/beranda.blade.php
+++ resources/views/layouts/beranda.blade.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
-    {{-- Memanggil Bootstrap --}}
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body>
-    <h1 class=" text-center text-primary">Hello</h1>
+    <h1>hello</h1>
 </body>
 </html>
